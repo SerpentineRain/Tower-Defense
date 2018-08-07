0396b65f97e5256e5f1cdc5f1a0303587cbda412
Danilee Dolloway <36463824+SerpentineRain@users.noreply.github.com>
2018-08-07 14:31:58 -0400

    Update Tower.cs

--- Tower.cs
+++ Tower.cs
@@ -1,7 +1,7 @@
-namespace IIITwilightWitchIII
+namespace CrowsCauldronDefense
 {
     //The tower class is a template for creating tower objects.
-    //The class can be called upon to create the object. 
+    //The class will be called upon to create the object. 
     class Tower
     {
        
