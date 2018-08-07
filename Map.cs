f4363c9ce40132b89d2f9e11d51acb92f6faf418
Danilee Dolloway <36463824+SerpentineRain@users.noreply.github.com>
2018-08-07 14:45:35 -0400

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -2,6 +2,10 @@ namespace CrowsCauldronDefense
 {
   class Map
   {
-    
+    public int Width;
+    private int Height;
+    //Accessibility levels determine which fields can be used outside of its class, and by what.
+    //Private fields are only accessible to metods in the class they're declared in.
+    //Public fields can be accessed by any method in any field.
   }
 }
