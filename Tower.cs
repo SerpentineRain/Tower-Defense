d59809ffe55dc4e02593055a3b6df4684676ebb0
Danilee Dolloway <36463824+SerpentineRain@users.noreply.github.com>
2018-08-07 14:24:01 -0400

    Create Tower.cs

---
+++ Tower.cs
@@ -0,0 +1,9 @@
+namespace IIITwilightWitchIII
+{
+    //The tower class is a template for creating tower objects.
+    //The class can be called upon to create the object. 
+    class Tower
+    {
+       
+    }
+}
