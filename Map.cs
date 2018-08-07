5ea42223130dd31de80d674108edc1643de4edcd
Danilee Dolloway <36463824+SerpentineRain@users.noreply.github.com>
2018-08-07 15:07:15 -0400

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -2,10 +2,17 @@ namespace CrowsCauldronDefense
 {
   class Map
   {
-    public int Width;
-    public int Height;
     //Accessibility levels determine which fields can be used outside of its class, and by what.
     //Private fields are only accessible to metods in the class they're declared in.
     //Public fields can be accessed by any method in any field.
+    public int Width;
+    public int Height;
+    //Constructor methods call constructors when an object is created.
+    //Any parameters added must be provided for the object to be constructed.
+    Map(int width, int height)
+    {
+      Width = width;
+      Height = height;
+    }
   }
 }
