fb1a8bc1aa5be2fc0ad2d1476d8d00edbf50c3f6
Danilee Dolloway <36463824+SerpentineRain@users.noreply.github.com>
2018-08-18 15:56:51 -0400

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -5,14 +5,25 @@ namespace CrowsCauldronDefense
     //Accessibility levels determine which fields can be used outside of its class, and by what.
     //Private fields are only accessible to metods in the class they're declared in.
     //Public fields can be accessed by any method in any field.
-    public int Width;
-    public int Height;
+    public readonly int Width;
+    public readonly int Height;
+    
     //Constructor methods call constructors when an object is created.
     //Any parameters added must be provided for the object to be constructed.
-    Map(int width, int height)
+    public Map(int width, int height)
     {
       Width = width;
       Height = height;
+    }
+    
+    public bool OnMap(Point point)
+    {
+      return point.X >= 0 && point.X < Width &&
+             point.Y >= 0 && point.Y < Height;
+      
+      //bool inBounds = !(point.X < 0 || point.X >= Width || point.Y < 0 || point.Y >= Height);
+      
+      //return inBounds;
     }
   }
 }
